50ab286eb790e7d97296f299d220e59d4c6bd501
Ken C <zhikang.chen@georgebrown.ca>
2022-01-28 21:27:54 -0500

    Added a function to ProjectileManager

--- Feral_Power/Assets/_Script/Player/ProjectileBehaviour.cs
+++ Feral_Power/Assets/_Script/Player/ProjectileBehaviour.cs
@@ -6,6 +6,7 @@ public class ProjectileBehaviour : MonoBehaviour
 {
 
     // Start is called before the first frame update
+    public Vector2 StartingPos;
     public Vector2 Dir;
     public float ProjectileSpeed;
 
@@ -17,11 +18,24 @@ public class ProjectileBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Physics2D.IgnoreLayerCollision(6,3, true);
         RB = GetComponent<Rigidbody2D>();
         SR = GetComponent<SpriteRenderer>();
     }
     public void MoveProjectile()
     {
         RB.velocity = Dir * ProjectileSpeed;
+    }
+
+    public void StopProjectile()
+    {
+        RB.velocity = Vector2.zero;
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.rigidbody.tag != "Player" && collision.rigidbody.tag != "PlayerProjectile")
+        {
+            transform.gameObject.SetActive(false);
+        }
     }
 }
