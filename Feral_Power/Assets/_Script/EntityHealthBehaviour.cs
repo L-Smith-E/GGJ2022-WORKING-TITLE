8a93c6625682f05a89742043e4bb15f058baea93
Ken C <zhikang.chen@georgebrown.ca>
2022-01-29 18:40:48 -0500

    Added Health behaviour, test scene, some prefab

---
+++ Feral_Power/Assets/_Script/EntityHealthBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealthBehaviour : MonoBehaviour
+{
+    public float CurrentHealth = 1;
+    public float MaxHealth = 1;
+    public bool IFrame = false;
+
+    // The duration counts in frame instead of second
+    // It's call I-FRAME for a reason
+    public int IFrameDuration = 30;
+    private int FrameSinceDamage = 0;
+    private void FixedUpdate()
+    {
+        if(FrameSinceDamage >= IFrameDuration)
+        {
+            IFrame = false;
+        }
+
+        if(IFrame)
+        {
+            FrameSinceDamage++;
+        }
+
+        if(CurrentHealth <= 0)
+        {
+            this.transform.gameObject.SetActive(false);
+        }
+    }
+
+    void TakeDamage(int DamageTaken)
+    {
+        if (!IFrame)
+        {
+            CurrentHealth -= DamageTaken;
+            IFrame = true;
+        }
+    }
+}
